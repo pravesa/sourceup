367acf3be626f97123c51e2015b008099b38bcb1
Praveen Saravanan <praveen747757@gmail.com>
2023-02-16 21:47:45 +0530

    feat(web): add AppLayout component

---
+++ client/web/src/AppLayout.tsx
@@ -0,0 +1,46 @@
+import {Menu} from '@mui/icons-material';
+import {CircularProgress, IconButton, Stack} from '@mui/material';
+import {Suspense, useState} from 'react';
+import {Outlet} from 'react-router-dom';
+import {SideBar, TitleBar} from './components';
+
+function AppLayout() {
+  const [openSideBar, setOpenSideBar] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    setOpenSideBar(true);
+  };
+
+  const handleClose = () => {
+    setOpenSideBar(false);
+  };
+
+  return (
+    <Stack direction="row" height="100%" bgcolor="#f4f6f8">
+      <Stack component="nav">
+        <SideBar open={openSideBar} onClose={handleClose}></SideBar>
+      </Stack>
+      <Stack flexGrow={1} alignItems="stretch" gap={2} overflow="hidden">
+        <TitleBar>
+          <IconButton
+            onClick={handleOpen}
+            sx={{display: {xs: 'inline-block', md: 'none'}}}
+          >
+            <Menu />
+          </IconButton>
+        </TitleBar>
+        <Suspense
+          fallback={
+            <Stack flexGrow={1} justifyContent="center" alignItems="center">
+              <CircularProgress disableShrink={true} />
+            </Stack>
+          }
+        >
+          <Outlet />
+        </Suspense>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default AppLayout;

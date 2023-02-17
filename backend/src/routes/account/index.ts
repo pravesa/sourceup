8047bcb4df071e653f27f1b0750126bc15aef210
Praveen Saravanan <praveen747757@gmail.com>
2023-02-17 08:00:49 +0530

    fix(backend): add authenticate middleware in signout

--- backend/src/routes/account/index.ts
+++ backend/src/routes/account/index.ts
@@ -2,11 +2,12 @@ import {Router} from 'express';
 import signinRoute from './signin';
 import signupRoute from './signup';
 import signoutRoute from './signout';
+import {authenticateSessionHandler} from '../../handlers';
 
 const accountRouter = Router();
 
 accountRouter.post('/signin', signinRoute);
 accountRouter.post('/signup', signupRoute);
-accountRouter.delete('/signout', signoutRoute);
+accountRouter.delete('/signout', authenticateSessionHandler, signoutRoute);
 
 export default accountRouter;

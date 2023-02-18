60b936099aa1ba965e87ccfff0c5d7422a5fc47f
Praveen Saravanan <praveen747757@gmail.com>
2023-02-18 12:00:12 +0530

    dev: add typings for users in database.d.ts

---
+++ client/web/src/types/database.d.ts
@@ -0,0 +1,46 @@
+export {};
+
+type Mail = {
+  pri: string;
+  sec?: Array<string>;
+};
+
+declare global {
+  // Address fields for user collection
+  type Address = {
+    bno: string;
+    st: string;
+    line1?: string;
+    line2?: string;
+    ldmk?: string;
+    city: string;
+    state: string;
+    country: string;
+    pncd: number;
+  };
+
+  // User collection fields
+  interface User {
+    _id: string;
+    name: string;
+    gstn: string;
+    pwd: string;
+    mail: Mail;
+    loc?: {
+      regd: Address;
+      head?: Address;
+      plant: {
+        [k: string]: Address;
+      };
+      warehouse?: {
+        [k: string]: Address;
+      };
+      other?: {
+        [k: string]: Address;
+      };
+    };
+    pref?: {
+      [k: string]: unknown;
+    };
+  }
+}
